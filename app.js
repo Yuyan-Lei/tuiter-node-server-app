11699a4ae493871f849cd10cce9c00816ad1019e
Yuyan-Lei <lei.yuy@northeastern.edu>
2023-03-24 00:12:51 -0700

    a8

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import cors from 'cors'
+
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 
 const app = express();
+
+// configure cors right after instantiating express
+app.use(cors())
 
 // parse JSON from HTTP request body
 app.use(express.json());
---
+++ src/service/tuits-service.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+const TUITS_API = 'http://localhost:4000/api/tuits';
+
+export const createTuit = async (tuit) => {
+    const response = await axios.get(TUITS_API);
+    const tuits = response.data;
+    return tuits;
+}
+
+export const findTuits  = async ()     => {}
+
+export const deleteTuit = async (tuit) => {}
+
+export const updateTuit = async (tuit) => {}
